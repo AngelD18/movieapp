41ab977aafa02acc32e0a421f14306106118e38d
AngelD18 <angel_18boca@hotmail.com>
2021-04-27 12:21:44 -0300

    push

--- src/screens/Movie.js
+++ src/screens/Movie.js
@@ -1,20 +1,25 @@
+
 import React, { Fragment, useEffect, useState } from 'react'
 import { StyleSheet, View, ScrollView, Image } from 'react-native'
 import { IconButton, Text, Title } from 'react-native-paper';
 import { getMovieByIdApi } from '../api/movie';
 import ModalVideo from '../components/ModalVideo';
 import { BASE_PATH_IMG } from '../utils/contants';
+import { map } from 'lodash';
+import { Rating } from 'react-native-ratings';
+import starDark from '../assets/png/starDark.png';
+import starLight from '../assets/png/starLight.png';
+import usePreferences from '../hook/usePreference'
 export default function Movie(props) {
   const { route } = props;
   const { id } = route.params;
 
   const [movie, setMovie] = useState({});
   const [show, setShow] = useState(false);
-  console.log(movie.poster_path)
+
   useEffect(() => {
     getMovieByIdApi(id).then((response) => {
       setMovie(response);
-
     });
   }, []);
 
@@ -24,11 +29,31 @@ export default function Movie(props) {
       <ScrollView>
         <MovieImage posterPath={movie.poster_path} />
         <MovieTrailer setShow={setShow} />
+        <MovieTitle movie={movie} />
+        <MovieRating voteCount={movie.vote_count} voteAverage={movie.vote_average} />
       </ScrollView>
-      <ModalVideo show={show} setShow={setShow} idMovie={id}/>
+      <ModalVideo show={show} setShow={setShow} idMovie={id} />
     </Fragment>
   )
 }
+
+function MovieTitle(props) {
+  const { movie } = props;
+
+  return (
+    <View style={styles.viewInfo}>
+      <Title>{movie.title}</Title>
+      <View style={styles.viewGenres}>
+        {map(movie.genres, (genre) => (
+          <Text key={genre.id} style={styles.genre}>
+            {genre.name}
+          </Text>
+        ))}
+      </View>
+    </View>
+  )
+}
+
 function MovieImage(props) {
   const { posterPath } = props;
 
@@ -52,6 +77,26 @@ function MovieTrailer(props) {
         size={30}
         style={styles.play}
         onPress={() => setShow(true)}
+      />
+    </View>
+  )
+}
+
+function MovieRating(props) {
+  const { voteCount, voteAverage } = props;
+  const media = voteAverage / 2;
+  const { theme } = usePreferences();
+
+  return (
+    <View style={styles.viewRating}>
+      <Rating
+        type="custom"
+        ratingImage={theme === 'dark' ? starDark : starLight}
+        ratingColor="#ffc205"
+        ratingBackgroundColor={theme === 'dark' ? '#192734' : '#f0f0f0'}
+        startingValue={media}
+        imageSize={20}
+        style={{ marginRight: 15 }}
       />
     </View>
   )
@@ -85,5 +130,21 @@ const styles = StyleSheet.create({
     width: 60,
     height: 60,
     borderRadius: 100
+  },
+  viewInfo: {
+    marginHorizontal: 30
+  },
+  viewGenres: {
+    flexDirection: 'row'
+  },
+  genre: {
+    marginRight: 20,
+    color: '#8697a5'
+  },
+  viewRating: {
+    marginHorizontal: 3,
+    marginTop: 10,
+    flexDirection: 'row',
+    alignItems: 'center'
   }
 })
